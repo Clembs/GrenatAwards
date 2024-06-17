85ec9da84fc548cb914764461b37e080760988a5
Gérald Leban <gerald.leban0@gmail.com>
2024-06-17 11:34:57 +0200

    feat: add category page with data about the said category

---
+++ src/routes/category/[id=integer]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageServerData } from './$types';
+
+  let { data }: { data: PageServerData } = $props();
+  const { category } = data;
+</script>
+
+<h1>{category.name}</h1>
+{#if category.description}
+  <p>{category.description}</p>
+{/if}
