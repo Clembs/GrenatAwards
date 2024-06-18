39af351e596e2ca88a568f759593aa8f0d2732e6
Gérald Leban <gerald.leban0@gmail.com>
2024-06-19 00:23:00 +0200

    feat: add open graph meta tags

---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  interface Props {
+    title: string;
+    description: string;
+
+    image: string;
+  }
+
+  const { title, description, image }: Props = $props();
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:site_name" content={title} />
+  <meta property="og:image" content={image} />
+  <meta property="og:image:width" content="1280" />
+  <meta property="og:image:height" content="624" />
+  <meta property="og:image:alt" content={title} />
+  <meta property="og:type" content="website" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content={image} />
+</svelte:head>
